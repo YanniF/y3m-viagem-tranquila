7aaaa88d80a4be5e2a1d0fa8659f7fc69f2f739c
yanni.fernanda@gmail.com <yanni.fernanda@gmail.com@46bd3481-17c6-2f65-0d11-ff98f50f54ed>
2013-06-22 04:51:21 +0000

    Desenvolvimento do Dicas de Viagem.

--- Y3M/src/br/com/fatecpg/y3mviagemtranquila/MainActivity.java
+++ Y3M/src/br/com/fatecpg/y3mviagemtranquila/MainActivity.java
@@ -2,14 +2,29 @@ package br.com.fatecpg.y3mviagemtranquila;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	Button btnDicas;
+	String dicas [];
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//vetor com as dicas
+		dicas = new String [] {"Aproveite as paradas durante a viagem para fazer breves caminhadas.",
+				"Procure levar na bagagem os medicamentos que você normalmente usa.",
+				"Antes de viajar, evite os excessos, as comidas gordurosas e a ingestão de alimentos com os quais não está habituado.",
+				"Mantenha seu corpo hidratado, bebendo líquidos durante o trajeto – mas não tome bebidas alcoólicas!"};
+		
+		btnDicas = (Button) findViewById(R.id.buttonDicas);//associando o botão da activty com o btnDicas
+		mostrarDicas();//chama o método mostrarDicas
 	}
 
 	@Override
@@ -17,6 +32,28 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	public void mostrarDicas() 
+	{//tratar para não mostrar a mesma dica várias vezes
+		btnDicas.setOnClickListener(new View.OnClickListener() {//coloca um evento no botão
+			
+			@Override
+			public void onClick(View v) 
+			{//sorteia um número que é escolhido para aparecer uma das mensagens
+				int n = (int) (Math.random() * dicas.length);
+				mostrarAlert("" + dicas[n]);
+			}
+		});
+	}
+	
+	//método para exibir o AlertDialog com a dica de viagem
+	public void mostrarAlert(String msg) 
+	{
+		AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);//cria um novo AlertDialog
+		alert.setMessage(msg);//insere mensagem do alert com o parâmetro (msg) desse método
+		alert.setNeutralButton("OK", null);//botão
+		alert.show();//mostra o AlertDialog
 	}
 
 }
