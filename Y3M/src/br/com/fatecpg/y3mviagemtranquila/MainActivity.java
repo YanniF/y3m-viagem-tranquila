50eee0c3710a92f6620e58c210af520984f480cc
yanni.fernanda@gmail.com <yanni.fernanda@gmail.com@46bd3481-17c6-2f65-0d11-ff98f50f54ed>
2013-06-22 05:47:19 +0000

    Desenvolvimento do "Dicas de Viagem".

--- Y3M/src/br/com/fatecpg/y3mviagemtranquila/MainActivity.java
+++ Y3M/src/br/com/fatecpg/y3mviagemtranquila/MainActivity.java
@@ -1,5 +1,7 @@
 package br.com.fatecpg.y3mviagemtranquila;
 
+import java.util.Random;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -11,6 +13,7 @@ public class MainActivity extends Activity {
 
 	Button btnDicas;
 	String dicas [];
+	int c = 0;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +21,20 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		//vetor com as dicas
-		dicas = new String [] {"Aproveite as paradas durante a viagem para fazer breves caminhadas.",
+		dicas = new String [] {
+				"Respeite os limites do seu carro. Excesso de carga e objetos soltos dentro do veículo representam um grande risco.",
+				"Aproveite as paradas durante a viagem para fazer breves caminhadas.",
 				"Procure levar na bagagem os medicamentos que você normalmente usa.",
+				"Verifique a validade dos documentos do carro.",
+				"Crianças de até sete anos e meio devem ser transportadas no banco de trás, com equipamento especial (cadeirinha).",
 				"Antes de viajar, evite os excessos, as comidas gordurosas e a ingestão de alimentos com os quais não está habituado.",
-				"Mantenha seu corpo hidratado, bebendo líquidos durante o trajeto – mas não tome bebidas alcoólicas!"};
+				"Dê atenção à pressão dos pneus, se não for o suficiente, o carro vai consumir mais combustível.",
+				"Mantenha seu corpo hidratado, bebendo líquidos durante o trajeto – mas não tome bebidas alcoólicas!",
+				"É possível que as crianças estejam com o cinto de segurança devidamente colocado e ainda assim confortáveis.",
+				"Se beber, não dirija."
+				};
+		
+		embaralharVetor(dicas);
 		
 		btnDicas = (Button) findViewById(R.id.buttonDicas);//associando o botão da activty com o btnDicas
 		mostrarDicas();//chama o método mostrarDicas
@@ -34,15 +47,37 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	static void embaralharVetor (String[] vet)
+	{//embaralha o vetor, assim não mostra sempre as dicas na mesma ordem
+	    Random r = new Random();
+	    String aux;
+	    int x;
+	    
+	    for (int i = (vet.length - 1); i >= 0; i--)
+	    {
+	      x = r.nextInt(i + 1);//sorteia um número
+	      //efetua a troca
+	      aux = vet[x];
+	      vet[x] = vet[i];
+	      vet[i] = aux;
+	    }
+	  }
+	
 	public void mostrarDicas() 
-	{//tratar para não mostrar a mesma dica várias vezes
+	{
 		btnDicas.setOnClickListener(new View.OnClickListener() {//coloca um evento no botão
 			
 			@Override
 			public void onClick(View v) 
-			{//sorteia um número que é escolhido para aparecer uma das mensagens
-				int n = (int) (Math.random() * dicas.length);
-				mostrarAlert("" + dicas[n]);
+			{
+				//pega a próxima mensagem do vetor e exibe
+				mostrarAlert("" + dicas[c]);
+				
+				c++;
+		        
+		        if(c == dicas.length) {
+		            c = 0;
+		        }
 			}
 		});
 	}
