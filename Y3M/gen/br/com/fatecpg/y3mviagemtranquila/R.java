54b18a3607f6ac06e3056a901c66526426b38cbe
mychella@fatecpg.com.br <mychella@fatecpg.com.br@46bd3481-17c6-2f65-0d11-ff98f50f54ed>
2013-06-25 06:27:27 +0000

    Atualização.

--- Y3M/gen/br/com/fatecpg/y3mviagemtranquila/R.java
+++ Y3M/gen/br/com/fatecpg/y3mviagemtranquila/R.java
@@ -24,40 +24,50 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080015;
-        public static final int btnVoltar=0x7f080013;
+        public static final int action_settings=0x7f08001d;
+        public static final int alcool=0x7f080013;
+        public static final int btnVoltar=0x7f08001c;
+        public static final int button1=0x7f080018;
         public static final int button190=0x7f080009;
         public static final int button191=0x7f08000a;
         public static final int button192=0x7f08000b;
         public static final int button193=0x7f08000c;
-        public static final int buttonCalcular=0x7f080008;
+        public static final int buttonCalcular=0x7f080006;
         public static final int buttonDicas=0x7f080011;
         public static final int buttonEmergencia=0x7f080010;
-        public static final int buttonGasto=0x7f08000e;
+        public static final int buttonGasto=0x7f08000f;
         public static final int buttonLimpar=0x7f080007;
-        public static final int buttonOpcao=0x7f08000f;
+        public static final int buttonOpcao=0x7f08000e;
         public static final int buttonVolta=0x7f08000d;
-        public static final int buttonVoltar=0x7f080000;
-        public static final int editTextKmLitro=0x7f080004;
+        public static final int buttonVoltar=0x7f080008;
+        public static final int cal=0x7f080016;
+        public static final int editTextKmLitro=0x7f080003;
         public static final int editTextKmViagem=0x7f080001;
-        public static final int editTextPrecoMedio=0x7f080006;
-        public static final int textView1=0x7f080014;
-        public static final int textViewKmLitro=0x7f080003;
-        public static final int textViewPrecoMedio=0x7f080005;
-        public static final int textViewTotalLitros=0x7f080002;
-        public static final int textViewValor=0x7f080012;
+        public static final int editTextPrecoMedio=0x7f080005;
+        public static final int gasolina=0x7f080015;
+        public static final int textView1=0x7f080012;
+        public static final int textView2=0x7f080014;
+        public static final int textViewKmLitro=0x7f080002;
+        public static final int textViewKmViagem=0x7f080019;
+        public static final int textViewPrecoMedio=0x7f080004;
+        public static final int textViewTotalLitros=0x7f080000;
+        public static final int textViewValor=0x7f08001a;
+        public static final int valAlcool=0x7f08001b;
+        public static final int voltar=0x7f080017;
     }
     public static final class layout {
         public static final int activity_gasto=0x7f030000;
         public static final int activity_ligacoes=0x7f030001;
         public static final int activity_main=0x7f030002;
-        public static final int activity_resultado_gasto=0x7f030003;
+        public static final int activity_melhor_opcao=0x7f030003;
+        public static final int activity_resultado_gasto=0x7f030004;
     }
     public static final class menu {
         public static final int gasto=0x7f070000;
         public static final int ligacoes=0x7f070001;
         public static final int main=0x7f070002;
-        public static final int resultado_gasto=0x7f070003;
+        public static final int melhor_opcao=0x7f070003;
+        public static final int resultado_gasto=0x7f070004;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -81,8 +91,14 @@ public final class R {
         public static final int texto3=0x7f05000c;
         public static final int title_activity_gasto=0x7f050007;
         public static final int title_activity_ligacoes=0x7f050013;
+        public static final int title_activity_melhor_opcao=0x7f050019;
         public static final int title_activity_resultado_gasto=0x7f050010;
         public static final int totalLitros=0x7f050011;
+        public static final int vlAl=0x7f05001c;
+        public static final int vlAlc=0x7f05001b;
+        public static final int vlAlcool=0x7f05001a;
+        public static final int vlGa=0x7f05001e;
+        public static final int vlGas=0x7f05001d;
         public static final int volta=0x7f050018;
         public static final int voltar=0x7f05000a;
     }
